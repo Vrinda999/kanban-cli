5253842d201a77d20ffb55f768155b033cde3e8a
Vrinda <abrolvrinda@gmail.com>
2025-03-31 22:04:31 +0530

    defined key bindings

---
+++ keys.go
@@ -0,0 +1,96 @@
+package main
+
+import "github.com/charmbracelet/bubbles/key"
+
+type keyMap struct {
+	New    key.Binding
+	Edit   key.Binding
+	Delete key.Binding
+	Up     key.Binding
+	Down   key.Binding
+	Left   key.Binding
+	Right  key.Binding
+	Enter  key.Binding
+	Help   key.Binding
+	Quit   key.Binding
+	Back   key.Binding
+}
+
+// Returns keybindings for the mini help view.
+// Both ShortHelp and FullHelp are Part of the key.Map interface.
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.Quit}
+}
+
+// Returns keybindings for the expanded help view.
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{ // 1st Col.
+			k.Up,
+			k.Down,
+			k.Left,
+			k.Right,
+		},
+		{ // 2nd Col.
+			k.Help,
+			k.Quit,
+		},
+	}
+}
+
+var keys = keyMap{
+	New: key.NewBinding(
+		key.WithKeys("n"),
+		key.WithHelp("n", "New"),
+	),
+
+	Edit: key.NewBinding(
+		key.WithKeys("e"),
+		key.WithHelp("e", "Edit"),
+	),
+
+	Delete: key.NewBinding(
+		key.WithKeys("backspace"),
+		key.WithHelp("backspace", "Delete"),
+	),
+
+	Up: key.NewBinding(
+		key.WithKeys("up", "w"),
+		key.WithHelp("↑/w", "Move Up"),
+	),
+
+	Down: key.NewBinding(
+		key.WithKeys("down", "s"),
+		key.WithHelp("↓/s", "Move Down"),
+	),
+
+	Left: key.NewBinding(
+		key.WithKeys("left", "a"),
+		key.WithHelp("←/a", "Move Right"),
+	),
+
+	Right: key.NewBinding(
+		key.WithKeys("right", "d"),
+		key.WithHelp("→/d", "Move Right"),
+	),
+
+	Enter: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "enter"),
+	),
+
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "Help"),
+	),
+
+	Quit: key.NewBinding(
+		key.WithKeys("q", "ctrl+c"),
+		key.WithHelp("q/ctrl+c", "Quit"),
+	),
+
+	Back: key.NewBinding(
+		key.WithKeys("esc"),
+		key.WithHelp("esc", "Back"),
+	),
+}
