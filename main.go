89930d639bdc3d7f39b5bed16dae2a23e9d7e1ef
Vrinda <abrolvrinda@gmail.com>
2025-04-01 18:32:03 +0530

    Refactor

--- main.go
+++ main.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 type status int
 
-const divisor = 4
+func (s status) getNext() status {
+	if s == done {
+		return todo
+	} else {
+		return s + 1
+	}
+}
+
+func (s status) getPrev() status {
+	if s == todo {
+		return done
+	} else {
+		return s - 1
+	}
+}
+
+const margin = 4
+
+var board *Board
 
 const (
 	todo status = iota
@@ -19,224 +35,17 @@ const (
 	done
 )
 
-/* Styling */
-var (
-	columnStyle = lipgloss.NewStyle().
-			Padding(1, 2)
-	focusedStyle = lipgloss.NewStyle().
-			Padding(1, 2).
-			Border(lipgloss.RoundedBorder()).
-			BorderForeground(lipgloss.Color("62"))
-	helpStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("241"))
-)
-
-// Custom Item
-type Task struct {
-	status      status
-	title       string
-	description string
-}
-
-// Moving a List Item to the Next Task
-func (t *Task) Next() {
-	if t.status == done {
-		t.status = todo
-	} else {
-		t.status++
-	}
-}
-
-// List.item interface implementation.
-func (t Task) FilterValue() string {
-	return t.title
-}
-
-func (t Task) Title() string {
-	return t.title
-}
-
-func (t Task) Description() string {
-	return t.description
-}
-
-// Main Model
-type Model struct {
-	loaded   bool // To make sure that list is not accessed before it's Initialised.
-	focused  status
-	lists    []list.Model
-	err      error
-	quitting bool
-}
-
-func New() *Model {
-	return &Model{}
-}
-
-func (m *Model) MoveToNext() tea.Msg {
-	selectedItem := m.lists[m.focused].SelectedItem()
-	selectedTask := selectedItem.(Task)
-	m.lists[selectedTask.status].RemoveItem(m.lists[m.focused].Index())
-	selectedTask.Next()
-	m.lists[selectedTask.status].InsertItem(len(m.lists[selectedTask.status].Items())-1, list.Item(selectedTask))
-
-	return nil
-}
-
-// Go To Next List
-func (m *Model) Next() {
-	if m.focused == done {
-		m.focused = todo
-	} else {
-		m.focused++
-	}
-}
-
-// Go To Previous List
-func (m *Model) Prev() {
-	if m.focused == todo {
-		m.focused = done
-	} else {
-		m.focused--
-	}
-}
-
-// On Startup we get a Window Size message, so we need Width and Height as Well.
-func (m *Model) initLists(width, height int) {
-	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height/2)
-	defaultList.SetShowHelp(false)
-	m.lists = []list.Model{defaultList, defaultList, defaultList}
-
-	// Init To Do
-	m.lists[todo].Title = "To Do"
-	m.lists[todo].SetItems([]list.Item{
-		Task{
-			status:      todo,
-			title:       "Buy Milk",
-			description: "Strawberry Milk",
-		},
-		Task{
-			status:      todo,
-			title:       "Eat Sushi",
-			description: "Miso Soup, and Rice",
-		},
-		Task{
-			status:      todo,
-			title:       "Fold Laundry",
-			description: "or Wear Wrinkly Tees",
-		},
-	})
-
-	// Init in Progress
-	m.lists[inProgress].Title = "In Progress"
-	m.lists[inProgress].SetItems([]list.Item{
-		Task{
-			status:      inProgress,
-			title:       "Go Proj",
-			description: "CLI Kanban",
-		},
-	})
-
-	// Init To Do
-	m.lists[done].Title = "Done"
-	m.lists[done].SetItems([]list.Item{
-		Task{
-			status:      done,
-			title:       "Portfolio",
-			description: "React, JS, Tailwind",
-		},
-	})
-
-}
-
-func (m Model) Init() tea.Cmd {
-	return nil
-}
-
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		if !m.loaded {
-			columnStyle.Width(msg.Width / divisor)
-			focusedStyle.Width(msg.Width / divisor)
-
-			columnStyle.Height(msg.Height - divisor)
-			focusedStyle.Height(msg.Height - divisor)
-
-			m.initLists(msg.Width, msg.Height)
-			m.loaded = true
-		}
-
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			m.quitting = true
-			return m, tea.Quit
-
-		case "left", "j":
-			m.Prev()
-
-		case "right", "l":
-			m.Next()
-
-		case "enter":
-			return m, m.MoveToNext
-		}
-	}
-
-	var cmd tea.Cmd
-	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
-	return m, cmd
-}
-
-func (m Model) View() string {
-	if m.quitting {
-		return ""
-	}
-
-	if m.loaded {
-		todoView := m.lists[todo].View()
-		inProgressView := m.lists[inProgress].View()
-		doneView := m.lists[done].View()
-
-		switch m.focused {
-		case inProgress:
-			return lipgloss.JoinHorizontal(
-				lipgloss.Left,
-				// .View() to get their String Rep.
-				columnStyle.Render(todoView),
-				focusedStyle.Render(inProgressView),
-				columnStyle.Render(doneView),
-			)
-
-		case done:
-			return lipgloss.JoinHorizontal(
-				lipgloss.Left,
-				// .View() to get their String Rep.
-				columnStyle.Render(todoView),
-				columnStyle.Render(inProgressView),
-				focusedStyle.Render(doneView),
-			)
-
-		default:
-			return lipgloss.JoinHorizontal(
-				lipgloss.Left,
-				// .View() to get their String Rep.
-				focusedStyle.Render(todoView),
-				columnStyle.Render(inProgressView),
-				columnStyle.Render(doneView),
-			)
-		}
-
-	} else {
-		return "loading..."
-	}
-
-}
-
 func main() {
-	m := New()
-	p := tea.NewProgram(m)
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close() // Close the file right before the Main function finishes execution
+
+	board = NewBoard()
+	board.initLists()
+	p := tea.NewProgram(board)
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
